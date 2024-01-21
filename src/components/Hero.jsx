d0a280c7495abbd8783d023cef7680efc32fc1fe
monish <monishbarse9@gmail.com>
2024-01-21 17:23:22 +0530

    git ignore

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
             Hi, I'm <span className='text-[#915EFF]'>Monish</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I Web Developer,  <br className='sm:block hidden' />
+            I am a Web Developer,  <br className='sm:block hidden' />
            Full Stack Developer
           </p>
         </div>
